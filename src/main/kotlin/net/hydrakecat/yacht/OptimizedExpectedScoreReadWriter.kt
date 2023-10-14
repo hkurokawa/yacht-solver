cbf01fe79ab6e13e11df9ecb68618271a4ffbc70
hkurokawa <hirosh.kurokawa@gmail.com>
2023-10-14 12:40:11 -0700

    Do save/load in background thread.

--- src/main/kotlin/net/hydrakecat/yacht/OptimizedExpectedScoreReadWriter.kt
+++ src/main/kotlin/net/hydrakecat/yacht/OptimizedExpectedScoreReadWriter.kt
@@ -1,5 +1,7 @@
 package net.hydrakecat.yacht
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.PrintWriter
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
@@ -10,37 +12,41 @@ import kotlin.io.path.reader
 import kotlin.io.path.writer
 
 object OptimizedExpectedScoreReadWriter {
+    private val backgroundDispatcher = Dispatchers.IO
+
     suspend fun save(fileName: String, expectedScore: (Set<Category>, Int) -> Double?) {
         val categories = Category.entries
         val path = Path.of(fileName)
         if (path.notExists()) {
             println("Saving to $fileName")
-            path.writer(
-                options = arrayOf(
-                    StandardOpenOption.CREATE_NEW,
-                    StandardOpenOption.WRITE
-                )
-            ).use { writer ->
-                val printer = PrintWriter(writer)
-                printer.println("## N: $N, M: $M, R: $R")
-                printer.println("## Categories: $categories")
-                printer.println("## Total upper section score to reach bonus: $UPPER_BONUS_MIN")
-                for (c in 0..<(1 shl categories.size)) {
-                    printer.print(
-                        String.format(
-                            "%${categories.size}s",
-                            Integer.toBinaryString(c)
-                        ).replace(" ", "0")
+            withContext(backgroundDispatcher) {
+                path.writer(
+                    options = arrayOf(
+                        StandardOpenOption.CREATE_NEW,
+                        StandardOpenOption.WRITE
                     )
-                    for (us in 0..UPPER_BONUS_MIN) {
-                        val d = expectedScore(c.toCategories(), us)
-                        if (d == null) {
-                            printer.printf(" %21s", Double.NaN)
-                        } else {
-                            printer.printf(" %21.17f", d)
+                ).use { writer ->
+                    val printer = PrintWriter(writer)
+                    printer.println("## N: $N, M: $M, R: $R")
+                    printer.println("## Categories: $categories")
+                    printer.println("## Total upper section score to reach bonus: $UPPER_BONUS_MIN")
+                    for (c in 0..<(1 shl categories.size)) {
+                        printer.print(
+                            String.format(
+                                "%${categories.size}s",
+                                Integer.toBinaryString(c)
+                            ).replace(" ", "0")
+                        )
+                        for (us in 0..UPPER_BONUS_MIN) {
+                            val d = expectedScore(c.toCategories(), us)
+                            if (d == null) {
+                                printer.printf(" %21s", Double.NaN)
+                            } else {
+                                printer.printf(" %21.17f", d)
+                            }
                         }
+                        printer.println()
                     }
-                    printer.println()
                 }
             }
         }
@@ -50,18 +56,20 @@ object OptimizedExpectedScoreReadWriter {
         val path = Path.of(fileName)
         if (path.exists()) {
             println("Loading from $fileName")
-            path.reader(options = arrayOf(StandardOpenOption.READ)).use { reader ->
-                val scanner = Scanner(reader)
-                var line: String
-                while (scanner.hasNextLine()) {
-                    line = scanner.nextLine()
-                    if (line.startsWith("#")) continue
-                    val numbers = line.split(Regex(" +"))
-                    val categories = Integer.parseInt(numbers[0], 2).toCategories()
-                    numbers.drop(1).forEachIndexed { us, s ->
-                        val d = s.toDouble()
-                        if (d.isFinite()) {
-                            onExpectedScore(categories, us, d)
+            withContext(backgroundDispatcher) {
+                path.reader(options = arrayOf(StandardOpenOption.READ)).use { reader ->
+                    val scanner = Scanner(reader)
+                    var line: String
+                    while (scanner.hasNextLine()) {
+                        line = scanner.nextLine()
+                        if (line.startsWith("#")) continue
+                        val numbers = line.split(Regex(" +"))
+                        val categories = Integer.parseInt(numbers[0], 2).toCategories()
+                        numbers.drop(1).forEachIndexed { us, s ->
+                            val d = s.toDouble()
+                            if (d.isFinite()) {
+                                onExpectedScore(categories, us, d)
+                            }
                         }
                     }
                 }
